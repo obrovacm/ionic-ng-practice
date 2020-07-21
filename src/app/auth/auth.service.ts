252627e3525965905cd92c929fc8635b8c70f588
Milos Obrovac <obrovac.milos@gmail.com>
2020-07-21 09:49:31 +0200

    local storage login token mock

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,6 +7,11 @@ export class AuthService {
   private _isAuthenticated = false;
 
   get isAuthenticated(){
+    if(!this._isAuthenticated){
+      if(localStorage.getItem('isAuthenticated') === 'true'){
+        this._isAuthenticated = true;
+      };
+    }
     return this._isAuthenticated;
   }
 
@@ -14,9 +19,11 @@ export class AuthService {
 
   login() {
     this._isAuthenticated = true;
+    localStorage.setItem('isAuthenticated', 'true');
   }
 
   logout() {
     this._isAuthenticated = false;
+    localStorage.setItem('isAuthenticated', 'false');
   }
 }
