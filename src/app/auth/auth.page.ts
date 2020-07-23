1f5188448049c80d890a6b3811e5368a0c68eda2
Milos Obrovac <obrovac.milos@gmail.com>
2020-07-23 11:06:58 +0200

    loading lock overlay /login

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "./auth.service";
 import { Router } from "@angular/router";
+import { LoadingController } from "@ionic/angular";
 
 @Component({
   selector: "app-auth",
@@ -8,12 +9,29 @@ import { Router } from "@angular/router";
   styleUrls: ["./auth.page.scss"],
 })
 export class AuthPage implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  // isLoading = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {}
 
   onLogin() {
+    // this.isLoading = true;
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: "Logging in..." })
+      .then((loadingEl) => {
+        loadingEl.present();
+
+        setTimeout(() => {
+          loadingEl.dismiss();
+          this.router.navigateByUrl("/places");
+          // this.isLoading = false;
+        }, 1500);
+      });
     this.authService.login();
-    this.router.navigateByUrl("/places");
   }
 }
