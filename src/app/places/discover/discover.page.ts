d648740f9eae56b82fb684da73a71b80fd2b8f7f
Milos Obrovac <obrovac.milos@gmail.com>
2020-07-23 10:52:41 +0200

    virtual-scroll & segment-button

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { SegmentChangeEventDetail } from "@ionic/core";
 import { PlacesService } from "../places.service";
 import { Place } from "../place.model";
 
@@ -9,10 +10,16 @@ import { Place } from "../place.model";
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(private placeService: PlacesService) {}
 
   ngOnInit() {
     this.loadedPlaces = this.placeService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
   }
 }
